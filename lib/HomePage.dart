10907840ab1511d2599306c082d67c649d74425d
Imtiyaz Akiwat <imtiyazakiwat0@gmail.com>
2024-03-11 09:22:22 +0530

    app icon changed , layout of homepage fixed

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -13,7 +13,6 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sharada Library'),
-        centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
@@ -37,130 +36,113 @@ class HomePage extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(15.0),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: SingleChildScrollView( // Wrap your content with SingleChildScrollView
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Card(
-                        elevation: 8.0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            SizedBox(height: 20.0),
-                            Image.asset(
-                              'assets/sharadhaLibImage.jpg',
-                              height: 150.0,
-                            ),
-                            SizedBox(height: 20.0),
-                            Text(
-                              'Welcome to Sharada Library',
-                              style: TextStyle(
-                                fontSize: 24.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                              child: Text(
-                                'Discover Your Perfect Study Haven: Tables Ready for Your Books and Ideas at Sharadha Library.',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(fontSize: 16.0),
-                              ),
-                            ),
-                            SizedBox(height: 30.0),
-                            ElevatedButton(
-                              onPressed: () {
-                                Navigator.pushReplacementNamed(context, '/booking');
-                              },
-                              child: Text(
-                                'Book a Table',
-                                style: TextStyle(fontSize: 18.0),
-                              ),
-                              style: ElevatedButton.styleFrom(
-                                padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 15.0),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 20.0),
-                            // Conditional rendering for admin
-                            if (_isAdmin(userEmail))
-                              Column(
-                                children: [
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.pushReplacementNamed(context, '/admin');
-                                    },
-                                    child: Text(
-                                      'View/Update Table Info',
-                                      style: TextStyle(fontSize: 18.0),
-                                    ),
-                                    style: ElevatedButton.styleFrom(
-                                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(30.0),
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(height: 20.0),
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => AdminApprovalPage()),
-                                      );
-                                    },
-                                    child: Text(
-                                      'Approve/Decline Requests',
-                                      style: TextStyle(fontSize: 18.0),
-                                    ),
-                                    style: ElevatedButton.styleFrom(
-                                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(30.0),
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(height: 20.0),
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => RenewalsPage()),
-                                      );
-                                    },
-                                    child: Text(
-                                      'Renewals',
-                                      style: TextStyle(fontSize: 18.0),
-                                    ),
-                                    style: ElevatedButton.styleFrom(
-                                      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(30.0),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                          ],
-                        ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20.0),
+                  Image.asset(
+                    'assets/sharadhaLibImage.jpg',
+                    height: 150.0,
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Welcome to Sharada Library',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      'Discover Your Perfect Study Haven: Tables Ready for Your Books and Ideas at Sharadha Library.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                  SizedBox(height: 30.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacementNamed(context, '/booking');
+                    },
+                    child: Text(
+                      'Book a Table',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 15.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 20.0),
+                  // Conditional rendering for admin
+                  if (_isAdmin(userEmail))
+                    Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(context, '/admin');
+                          },
+                          child: Text(
+                            'View/Update Table Info',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => AdminApprovalPage()),
+                            );
+                          },
+                          child: Text(
+                            'Approve/Decline Requests',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => RenewalsPage()),
+                            );
+                          },
+                          child: Text(
+                            'Renewals',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
             ),
-
           ),
         ),
       ),
@@ -190,7 +172,8 @@ class HomePage extends StatelessWidget {
 
     // Check if the current user is signed in with Firebase
     final currentUser = FirebaseAuth.instance.currentUser;
-    if (currentUser != null && (currentUser.email == 'libraryadmin@gmail.com' || currentUser.email == 'ghorpadesudheer@gmail.com')) {
+    if (currentUser != null &&
+        (currentUser.email == 'libraryadmin@gmail.com' || currentUser.email == 'ghorpadesudheer@gmail.com')) {
       return true;
     }
 
